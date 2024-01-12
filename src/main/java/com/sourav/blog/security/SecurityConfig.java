1967998737d40e0568e9b7b51e9224b43f794776
Sourav Singh <souravsingh2609@gmail.com>
2024-01-13 04:02:05 +0530

    Security endpoints.

--- src/main/java/com/sourav/blog/security/SecurityConfig.java
+++ src/main/java/com/sourav/blog/security/SecurityConfig.java
@@ -3,15 +3,22 @@ package com.sourav.blog.security;
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 import com.sourav.blog.services.impl.UserDetailsServiceImpl;
+import com.sourav.blog.token.JwtAuthenticationEntryPoint;
 
 
 @Configuration
@@ -19,6 +26,10 @@ import com.sourav.blog.services.impl.UserDetailsServiceImpl;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+//	@Autowired
+//	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
 
@@ -31,8 +42,22 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+
+		http.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/api/v1/auth/login")
+						.permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+				.authenticationProvider(authenticationProvider());
+//				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 
